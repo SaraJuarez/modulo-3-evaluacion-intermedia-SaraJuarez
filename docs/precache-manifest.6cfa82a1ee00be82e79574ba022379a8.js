207826c3b272a93a288ae1f7b76c34f4cd6a46ae
adalaber <sarajuarezzarco@gmail.com>
2020-04-07 10:10:57 +0200

    Add functioning web without finished styles

---
+++ docs/precache-manifest.6cfa82a1ee00be82e79574ba022379a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe70244c3a21587de3b7113f3d79892",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9dce733ceb6d12326c22",
+    "url": "./static/css/main.8ee518fb.chunk.css"
+  },
+  {
+    "revision": "b4ccc13ad022e1ef9d23",
+    "url": "./static/js/2.1a14a13b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1a14a13b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dce733ceb6d12326c22",
+    "url": "./static/js/main.357f2459.chunk.js"
+  },
+  {
+    "revision": "16bc10e28e3e3135bc54",
+    "url": "./static/js/runtime-main.1127966d.js"
+  }
+]);
